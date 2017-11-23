d2160828799a8fba5cef8f5f6e3f8e66e04e0f29
zaru@sakuraba <zarutofu@gmail.com>
2017-11-24 02:43:13 +0900

    🐎

--- kamatakun.go
+++ kamatakun.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+
 	"github.com/zaru/kamatakun/gogoa"
 )
 
 func main() {
 
-	app := gogoa.SharedApplication()
-	window := gogoa.NewWindow()
-	window.MakeKeyAndOrderFront()
-	app.Run()
-	// if len(os.Args) == 1 {
-	// 	cmd := exec.Command(os.Args[0], "--child")
-	// 	cmd.Start()
-	// } else {
-	// 	for {
-	// 		app := gogoa.SharedApplication()
-	// 		window := gogoa.NewWindow()
-	// 		window.MakeKeyAndOrderFront()
-	// 		app.Run()
-	// 	}
-	// }
+	// app := gogoa.SharedApplication()
+	// window := gogoa.NewWindow()
+	// window.MakeKeyAndOrderFront()
+	// app.Run()
+	if len(os.Args) == 1 {
+		cmd := exec.Command(os.Args[0], "--child")
+		cmd.Start()
+	} else {
+		for {
+			app := gogoa.SharedApplication()
+			window := gogoa.NewWindow()
+			window.MakeKeyAndOrderFront()
+			app.Run()
+		}
+	}
 }
